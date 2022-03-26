37607bf0d748643e64e3d92f8ab5cd70258cb210
Ashley Mattam <amattam427@gmail.com>
2022-03-25 21:20:54 -0400

    table styling

--- client/src/components/BreweryItem.js
+++ client/src/components/BreweryItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { TableContainer, TableRow } from '@mui/material';
+import { Table } from '@mui/material';
+import { TableCell } from '@mui/material';
+import { TableBody } from '@mui/material';
+import { Paper } from '@mui/material';
 
 
 function BreweryItem({id, name, street, city, state, phone}){
 
     return (
         <div className = "brewery-table">
-            <table>
+            <TableContainer component={Paper}>
+                <Table  aria-label="simple table">
+                    <TableBody>
+                        <TableRow   >
+                            <TableCell >{name}</TableCell>
+                            <TableCell >{street}</TableCell>
+                            <TableCell >{city}</TableCell>
+                            <TableCell >{state}</TableCell>
+                            <TableCell >{phone}</TableCell>
+                        </TableRow>
+                    </TableBody>
+
+
+                </Table>
+            </TableContainer>
+            {/* <table>
                 <tbody>
                     <tr>
                         <td>{name}</td>
@@ -15,7 +35,7 @@ function BreweryItem({id, name, street, city, state, phone}){
                         <td>{phone}</td>
                     </tr>
                 </tbody>
-            </table>
+            </table> */}
 
         </div>
     )
--- client/src/components/BreweryList.js
+++ client/src/components/BreweryList.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { TableContainer } from '@mui/material';
+import { Table } from '@mui/material';
+import { TableRow } from '@mui/material';
+import { TableCell } from '@mui/material';
+import { TableHead } from '@mui/material';
 import BreweryItem from './BreweryItem';
 
 
@@ -8,7 +13,20 @@ function BreweryList({brewery}){
         <div>
             <h1>Find a Brewery</h1>
             <div className = "table-header">
-            <table>
+                <TableContainer>
+                    <Table sx={{minWidth: 500}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell >Brewery</TableCell>
+                                <TableCell >Street</TableCell>
+                                <TableCell >City</TableCell>
+                                <TableCell >State</TableCell>
+                                <TableCell >Phone Number</TableCell>
+                            </TableRow>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
+            {/* <table>
                 <thead>
                         <tr>
                             <th>Brewery</th>
@@ -18,7 +36,7 @@ function BreweryList({brewery}){
                             <th>Phone Number</th>
                         </tr>
                     </thead>
-                </table>
+                </table> */}
 
             </div>
             {brewery.map((brew)=>{
