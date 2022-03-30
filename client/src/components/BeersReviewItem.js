4aea07a5aad17b55db088d7b7a90d2991eca8db8
Ashley Mattam <amattam427@gmail.com>
2022-03-30 05:16:50 -0400

    css third commit

--- client/src/components/BeersReviewItem.js
+++ client/src/components/BeersReviewItem.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
 import BeersReviewEdit from './BeersReviewEdit';
 
+
 function BeersReviewItem({onUpdateReview, onDeleteReview, reviewId, review, author, rating}){
-    // const [updatedComment, setUpdatedComment] = useState('');
-    // const [updatedName, setUpdatedName] = useState('');
-    // const [updatedRating, setUpdatedRating] = useState(0);
+   
     const [editComment, setEditComment] = useState(false)
 
     function handleDelete(){
@@ -32,8 +31,11 @@ function BeersReviewItem({onUpdateReview, onDeleteReview, reviewId, review, auth
                 <h2>{review}</h2>
                 <h3>Rating out of 5: {rating}</h3>
                 <h4>-{author}</h4>
-                <button onClick={handleDelete}>Delete</button>
-                <button onClick={()=>setEditComment((editComment)=> !editComment)}>Edit</button>
+                <div className="review-btn">
+                <button className="delete-btn" onClick={handleDelete}>Delete</button>
+                <button className="edit-btn" onClick={()=>setEditComment((editComment)=> !editComment)}>Edit</button>
+                </div>
+    
                 </div>
             )
             }
